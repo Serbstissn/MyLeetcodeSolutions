a7cf6d5bf152a7788823ab43483396303a140d70
Serbstissn <sebastiangizka@gmail.com>
2022-02-12 13:42:35 +0100

    Initial commit

---
+++ src/main/java/com/sebcio/leet_questions/LeetQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.sebcio.leet_questions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetQuestionsApplication.class, args);
+	}
+
+}
